b214186ad10369f2bb621614e20ad355226ad8e0
k4wataru <k.wataru1234@gmail.com>
2024-05-15 12:48:37 +0000

    Practice

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,15 +2,17 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
     @list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     if @list.save
+      flash[:notice] = "投稿に成功しました。"
     # 4. トップ画面へリダイレクト
       redirect_to list_path(@list.id)
     else
+      flash.now[:notice] = "投稿に失敗しました。"
       render :new
     end
   end
@@ -26,21 +28,21 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
     redirect_to '/lists'
   end
-  
+
   private
-  
+
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
